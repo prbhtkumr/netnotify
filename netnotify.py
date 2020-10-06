065f51bb2ad6609462341edd0433f4682cbc9f41
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2020-10-06 19:36:36 +0530

    Update netnotify.py

--- netnotify.py
+++ netnotify.py
@@ -1,27 +1,24 @@
 # a simple program to notify you whhen you're back online
 
-import os
-import sys
-from datetime import datetime
+import os #for system commands
+from datetime import datetime  #for noting the time
 
 
 def check():
 	site = "google.com" #your website of choice
 	while True:
 		if os.system("ping "+site) == 0:
-			print("pass")
-			print("device came online at "+str(datetime.now()))
+			print(" pass \n device came online at "+str(datetime.now()))
 			play()
 			break
 		else:
-			sys.stdout.write("fail \n as of "+str(datetime.now()))
-			sys.stdout.flush()
+			print(" fail \n as of "+str(datetime.now()))
 			os.system("cls")
 
 
 
 def play():
-	os.system('F: && cd \\Music\\Soft Jazz\\ && "02. Fallin\'.mp3"') #your music of choice
+	os.system('F: && cd \\Music\\Soft Jazz\\ && "02. Fallin\'.mp3"') #your music file of choice
 
 if __name__ == '__main__':
 	check()
