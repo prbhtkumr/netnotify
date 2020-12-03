810b87d2f912b304e7a6d7bc4254dd6597f2d421
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2020-12-03 20:17:10 +0530

    added notification fuctitonality

--- netnotify.py
+++ netnotify.py
@@ -1,8 +1,9 @@
-# a simple program to notify you whhen you're back online
+# a simple program to notify you when you're back online
 
 import os #for system commands
 import datetime	#for noting the time
 from time import time
+from plyer import notification 
 
 start = time()
 
@@ -13,25 +14,31 @@ def check():
 		if os.system("ping "+site) == 0:
 			print("\n\n pass \n device came online at "+str(datetime.datetime.now()))
 			time_passed()
+			notify()
+			print(" time passed: {}".format(time_passed))
 			play()
 			break
 		else:
 			print("\n\n fail \n as of "+str(datetime.datetime.now()))
 			time_passed()
+			print(" time passed: {}".format(time_passed))
+			notify()
 			os.system("cls")
-
 
 def time_passed():
 	end = time()
-	print("\n	time passed: ",datetime.timedelta(seconds=int(end-start)))
-
+	global time_passed
+	time_passed = datetime.timedelta(seconds=int(end-start))
+	return time_passed
 def play():
 	os.system('F: && cd \\Music\\Soft Jazz\\ && "02. Fallin\'.mp3"') #your music file of choice
 
+def notify():
+	notification.notify( 
+		title = "Your device is back online", 
+		message = ("Time Offline: {}".format(time_passed)),
+		timeout = 5)
 
 if __name__ == '__main__':
 	check()
-
-
-
 
